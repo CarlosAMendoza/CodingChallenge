8480572f3e6f766c9ef221ef897941fffe25db58
Carlos Mendoza <1cmendoza1996@gmail.com>
2019-08-23 21:37:05 -0400

    Initial Commit

---
+++ Coding Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coding Challenge
+//
+//  Created by Carlos Mendoza on 8/23/19.
+//  Copyright © 2019 Carlos Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
